7146bc71579f1049348d1321ff1f6b41768b7157
LR <36897155+ProfISN@users.noreply.github.com>
2020-12-11 16:00:33 +0100

    Add files via upload

---
+++ 1_codes ASCII des lettres mauscules ELEVE.py
@@ -0,0 +1,67 @@
+"""
+Ce programme:  Affiche les codes ASCII des lettres MAJUSCULES
+
+Auteur:
+Date: nov 2020
+
+Entrée:
+Résultat:   Affiche en décimal des codes ASCII des lettres MAJUSCULES
+            ...
+            puis
+            Affiche les caractères correspondants aux codes ASCII des lettres MAJUSCULES
+            A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
+
+"""
+
+# 1_Se documenter sur le codage des caractères en ASCII
+"""
+Voir cours et table ASCII étendue sur Moodle
+
+
+
+"""
+
+# 2_Dans la table ASCII ou la table ASCII étendue, repérer le code HEXADECIMAL
+# du caractère A et celui du caractère Z
+#            A -->                Z -->
+
+# 3_Mémoriser le code ASCII hexadécimal du caractère A dans une variable A_ASCII
+# A_ASCII=____
+
+# 4_Afficher la valeur de la variable A_ASCII en décimal: 
+
+# 5_Pour afficher la valeur de la variable x en hexadécimal
+# print( ' A_ASCII en hexa vaut Ox', format(___, '02X'))
+
+
+
+# 6_Mémoriser le code ASCII du caractère Z dans une variable n
+# Z_ASCII=____
+
+# 7_Afficher la valeur de la variable Z_ASCII en hexadécimal et en décimal
+
+
+
+
+print("\nAffichage en décimal des codes ASCII des lettres MAJUSCULES")
+
+# 8_Avec une boucle for, afficher en décimal les codes ASCII des lettres
+# majuscules  de A à Z séparées par un espace ' '
+# Pour i allant de x à n compris avec un pas de 1 faire
+#      Afficher (i) puis un espace
+
+
+
+
+# 9_Afficher le caractère corresponadant au code ASCII 66 --> voir fonction chr()
+# Dans EduPython voir Aide --> Manuel Python --> rechercher chr puis Built-in functions
+# print ('\n\nle caractère correspondant au code ASCII 66 (0x42) est' , _______)
+
+
+print("\nAffichage des caractères correspondants aux codes ASCII des lettres MAJUSCULES")
+
+# 10_Afficher les codes ASCII en DECIMAL des lettres de A à Z
+# Pour i allant de x à n compris avec un pas de 1 faire
+#       convertir le code ASCII en caractère puis afficher le caractère suivi d'un espace
+
+
